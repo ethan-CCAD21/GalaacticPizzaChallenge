067e4372bcc1101b25002a21f636e4acb6df89cf
ethan-CCAD21 <ethan.d.moore@outlook.com>
2025-11-06 13:49:25 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GalacticPizzaChallenge;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string userInput = "";
+
+        Console.WriteLine("Welcome to Galactic Pizza!\n");
+
+        Console.WriteLine("What is the name for your order?");
+        Console.Write("Name: ");
+    }
+}
